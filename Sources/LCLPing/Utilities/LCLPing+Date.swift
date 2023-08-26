df41160b9a34c4e0223ed25a5b44589dee4bdedc
Zhennan Zhou <zhouz46@uw.edu>
2023-08-26 00:26:13 -0700

    update runtime platform check

--- Sources/LCLPing/Utilities/LCLPing+Date.swift
+++ Sources/LCLPing/Utilities/LCLPing+Date.swift
@@ -11,7 +11,7 @@ extension Date {
     
     /// Get the current timestamp, in second, since  00:00:00 UTC on 1 January 1970
     static var currentTimestamp: TimeInterval {
-        if #available(macOS 12, *) {
+        if #available(macOS 12, iOS 15, tvOS 15, watchOS 8, *) {
             return Date.now.timeIntervalSince1970
         } else {
             // Fallback on earlier versions
